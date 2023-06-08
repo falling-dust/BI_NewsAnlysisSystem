0ba1fa8c1771f9971aa4c56102396c76867bce63
Muzukizh <106598067+Muzukizh@users.noreply.github.com>
2023-06-09 00:47:00 +0800

    修改popularity接口形式

--- BI_BackEnd/src/main/java/com/tj/bi_backend/controller/HistoryController.java
+++ BI_BackEnd/src/main/java/com/tj/bi_backend/controller/HistoryController.java
@@ -2,6 +2,8 @@ package com.tj.bi_backend.controller;
 
 import com.tj.bi_backend.entity.CategoryInterest;
 import com.tj.bi_backend.entity.CategoryPopularity;
+import com.tj.bi_backend.entity.DTO.CPDTO;
+import com.tj.bi_backend.entity.DTO.NPDTO;
 import com.tj.bi_backend.entity.NewsPopularity;
 import com.tj.bi_backend.entity.UserInterest;
 import com.tj.bi_backend.result.Result;
@@ -10,12 +12,14 @@ import com.tj.bi_backend.service.ICPService;
 import com.tj.bi_backend.service.INPService;
 import com.tj.bi_backend.service.IUIService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import static com.tj.bi_backend.utils.TimeUtils.timeTransfer;
 
 @RestController
 @RequestMapping("/history")
@@ -34,20 +38,41 @@ public class HistoryController {
 
     @GetMapping("/news-popularity")
     public Result getNewsPopularityByNewsId(@RequestParam String newsId){
-        List<NewsPopularity> npList = npService.getByNewsId(newsId);
+        List<NewsPopularity> npList = npService.getByNewsId(newsId);  //根据新闻id获取数据
         if(npList.isEmpty()){
             return Result.error();
         }
-        return Result.success(npList);
+
+        List<NPDTO> resultList = new ArrayList<>();  //将数据处理为前端需要的格式
+        for(NewsPopularity np : npList){
+            NPDTO npdto = new NPDTO();
+            npdto.setDate(timeTransfer(np.getDate()));
+            npdto.setClickTimes(np.getClickTimes());
+            resultList.add(npdto);
+        }
+        return Result.success(resultList);
     }
 
     @GetMapping("/category-popularity")
-    public Result getCategoryPopularity(){
-        List<CategoryPopularity> cpList = cpService.list();
+    public Result getCategoryPopularity(
+            @RequestParam("startTime") Date startTime,
+            @RequestParam("endTime") Date endTime
+    ){
+        List<CategoryPopularity> cpList = cpService.getByTime(startTime, endTime);  //获取所有的类别兴趣度数据
         if(cpList.isEmpty()){
             return Result.error();
         }
-        return Result.success(cpList);
+
+        List<CPDTO> resultList = new ArrayList<>();  //将数据处理为前端需要的格式
+        for(CategoryPopularity cp : cpList){
+            CPDTO cpdto = new CPDTO();
+            cpdto.setCategory(cp.getCategory());
+            cpdto.setDate(timeTransfer(cp.getDate()));
+            cpdto.setClickTimes(cp.getClickTimes());
+            resultList.add(cpdto);
+        }
+
+        return Result.success(resultList);
     }
 
     @GetMapping("/user-interest")
--- BI_BackEnd/src/main/java/com/tj/bi_backend/service/impl/CPService.java
+++ BI_BackEnd/src/main/java/com/tj/bi_backend/service/impl/CPService.java
@@ -1,5 +1,6 @@
 package com.tj.bi_backend.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tj.bi_backend.entity.CategoryPopularity;
 import com.tj.bi_backend.mapper.CPMapper;
@@ -7,8 +8,17 @@ import com.tj.bi_backend.service.ICPService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+
 @Service
 public class CPService extends ServiceImpl<CPMapper, CategoryPopularity> implements ICPService {
     @Resource
     private CPMapper cpMapper;
+    @Override
+    public List<CategoryPopularity> getByTime(Date startTime, Date endTime){
+        QueryWrapper<CategoryPopularity> wrapper = new QueryWrapper<>();
+        wrapper.between("date", startTime, endTime);
+        return list(wrapper);
+    }
 }
