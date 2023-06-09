04d771c836b6e48ccae9f7bd2a4923f7c7dc5323
Muzukizh <106598067+Muzukizh@users.noreply.github.com>
2023-06-09 22:42:44 +0800

    球球github让我把时间传上去

--- BI_BackEnd/src/main/java/com/tj/bi_backend/controller/HistoryController.java
+++ BI_BackEnd/src/main/java/com/tj/bi_backend/controller/HistoryController.java
@@ -90,7 +90,7 @@ public class HistoryController {
         Map<String, List<InterestDTO>> resultList = new HashMap<>();  //将数据处理为前端需要的格式，此处用字典存储
         for(UserInterest ui : uiList){
             InterestDTO tmp = new InterestDTO();  //新建要插入的数据
-            tmp.setDate(timeTransfer(ui.getDate()));
+            tmp.setDate(timestampToString(ui.getDate()));
             tmp.setInterestClicks(ui.getInterestClicks());
 
             String category = ui.getCategory();  //判断类型是否在字典的关键词里，如果在则直接插入，如果没有则新建列表后插入
@@ -116,7 +116,7 @@ public class HistoryController {
         Map<String, List<InterestDTO>> resultList = new HashMap<>();  //将数据处理为前端需要的格式，此处用字典存储
         for(CategoryInterest ci : ciList){
             InterestDTO tmp = new InterestDTO();  //新建要插入的数据
-            tmp.setDate(timeTransfer(ci.getDate()));
+            tmp.setDate(timestampToString(ci.getDate()));
             tmp.setInterestClicks(ci.getInterestClicks());
 
             String category = ci.getCategory();  //判断类型是否在字典的关键词里，如果在则直接插入，如果没有则新建列表后插入
