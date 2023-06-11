675f8d2302bf46e3180d59c2ad7f7e615ea5c479
Muzukizh <106598067+Muzukizh@users.noreply.github.com>
2023-06-11 18:03:36 +0800

    添加新闻复合查询

--- BI_BackEnd/src/main/java/com/tj/bi_backend/entity/News.java
+++ BI_BackEnd/src/main/java/com/tj/bi_backend/entity/News.java
@@ -1,9 +1,11 @@
 package com.tj.bi_backend.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class News {
+    @TableId(value = "news_id")
     private String newsId;
     private String category;
     private String topic;
